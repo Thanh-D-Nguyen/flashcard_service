d4a938fee919071d6d6f7e60911e60c78c823862
Thanhnv <thanhnv.ip@gmail.com>
2021-11-07 19:48:52 +0900

    add upload function

--- src/main/java/com/app/Flashcard/FlashcardApplication.java
+++ src/main/java/com/app/Flashcard/FlashcardApplication.java
@@ -1,8 +1,14 @@
 package com.app.Flashcard;
 
+import com.app.Flashcard.service.StorageService;
+import com.app.Flashcard.storage.StorageProperties;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
+@EnableConfigurationProperties(StorageProperties.class)
 @SpringBootApplication
 public class FlashcardApplication {
 
@@ -10,4 +16,11 @@ public class FlashcardApplication {
 		SpringApplication.run(FlashcardApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
